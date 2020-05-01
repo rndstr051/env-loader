14fb67c27bad3e9f96f49df89e4781026513b466
rndstr051 <rndstr051-openshift@yahoo.de>
2020-05-01 19:15:33 +0000

    Initial

---
+++ src/env-loader.js
@@ -0,0 +1,36 @@
+module.exports = function(content) {
+    return undefined;
+};
+module.exports.pitch = function(remainingRequest, precedingRequest, data) {
+
+    this.addDependency(this.resourcePath);
+
+    const fileContent = this.fs.readFileSync(this.resourcePath).toString();
+    console.info(fileContent);
+    const outerMatch = fileContent.match(/export\s+module\s+(?<name>[^\s]+)\s*{(?<inner>[^}]*)}/);
+    console.info(JSON.stringify(outerMatch));
+    const innerRegex = /const\s+(?<envname>[^\s:]+):(?<envtype>[^\s;]+)\s*;/g;
+    const innerString = outerMatch.groups['inner'];
+
+    let result = {};
+    let innerMatch = innerRegex.exec(innerString);
+    while (innerMatch != null) {
+        if (innerMatch.groups['envtype'] == 'string') {
+        result[innerMatch.groups['envname']] = String(process.env[innerMatch.groups['envname']]);
+        } else if (innerMatch.groups['envtype'] == 'number') {
+        result[innerMatch.groups['envname']] = Number(process.env[innerMatch.groups['envname']]);
+        } else if (innerMatch.groups['envtype'] == 'boolean') {
+        result[innerMatch.groups['envname']] = Boolean(process.env[innerMatch.groups['envname']]);
+        } else {
+        result[innerMatch.groups['envname']] = process.env[innerMatch.groups['envname']];
+        }
+
+
+        innerMatch = innerRegex.exec(innerString);
+    }
+
+    let resultOuter = {};
+    resultOuter[outerMatch.groups['name']] = result;
+
+    return 'module.exports = ' + JSON.stringify(resultOuter) + ';';
+};
